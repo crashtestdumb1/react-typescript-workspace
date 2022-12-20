ed0350caf7cac1896debb76338c8f8b54c0a7c81
Ray Raspberry <ray@yfoundry.io>
2022-12-19 23:09:58 -0600

    homepage wipe

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,11 +1,9 @@
+import styles from "@scss/app.module.scss";
+import Logo from '@images/logo.svg';
+
 export default function AppContent() {
   return (
     <>
-      <p>
-        <code>
-          Welcome
-        </code>
-      </p>
     </>
   );
 }
